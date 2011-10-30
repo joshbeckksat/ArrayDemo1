ddbc04d32e524e020ff592c1c201fe018b36a9d5
josh <josh@josh-EP45-UD3P>
2011-10-30 09:39:47 -0500

    Initial_Commit

---
+++ ArrayDemo1/src/com/beck/ArrayDemo1/ArrayDemo1.java
@@ -0,0 +1,29 @@
+package com.beck.ArrayDemo1;
+
+public class ArrayDemo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// This is designed to show you how simple arrays work. You will seem them a lot.
+		
+		//First, we declare an array variable
+		int[] myarray = new int[6];
+		//in this case we are allotting 6 spaces in our array.
+		//arrays always start with index zero. 
+		//Now we can populate this array with simple integer values
+		myarray[0] = 10;
+		myarray[1] = 20;
+		myarray[2] = 30;
+		myarray[3] = 40;
+		myarray[4] = 50;
+		myarray[5] = 60;
+		
+		//assignment: print the values of each array index using 6 System.out.println statments.
+		
+		
+
+	}
+
+}
